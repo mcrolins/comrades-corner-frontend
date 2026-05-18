a437903398ac3b90e07a5fa00719bdfb4d1b6607
mcrolins <rolinsmac21@gmail.com>
2026-05-18 09:04:25 +0300

    chore: update API_BASE default to localhost for consistency

--- lib/adminApi.ts
+++ lib/adminApi.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000";
 
 async function apiFetch(path: string, options: RequestInit = {}) {
   const url = `${API_BASE}${path}`;
